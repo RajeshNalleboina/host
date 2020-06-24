2afde003eb98031c5bfbece318409cafa8fca4cc
RajeshNalleboina <rajeshnalleboina94@gmail.com>
2020-06-24 21:13:36 +0530

    Updates

---
+++ precache-manifest.9e70102fd3b4c17f6b766f6d77b55853.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e3d25a3c194faba512a0658e3e4e1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db7800645dfc757f7049",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb77fd248ed01988a51f",
+    "url": "/static/js/2.ae58fdd0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ae58fdd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db7800645dfc757f7049",
+    "url": "/static/js/main.01ebcfe3.chunk.js"
+  },
+  {
+    "revision": "84659c7cf390551e69fa",
+    "url": "/static/js/runtime-main.8879a247.js"
+  },
+  {
+    "revision": "6f88fc6b60d4b7515f1b0d14efd320a0",
+    "url": "/static/media/profile.6f88fc6b.svg"
+  }
+]);
